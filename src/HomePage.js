4990d1f8a933f9b708175664214bd791d09aca4d
Christine Jeon <csjeon28@gmail.com>
2021-06-01 02:31:49 -0400

    added image and welcome & login message

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,20 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './HomePage.css';
 
 class HomePage extends Component {
     handleSubmit(e) {
         e.preventDefault();
     }
 
-    handleUsernameChange(e) {
+    handleUsername(e) {
 
     }
 
     render() {
         return (
             <div className='homepage'>
+                <div className='homepage-welcome'>
+                    Welcome to the Friends Trivia Game!
+                </div>
+                <div className='homepage-image'>
+                    <img src='/images/Friends door.jpeg' alt='Friends door'></img>
+                </div>
                 <form onSubmit={this.handleSubmit}>
+                    <p>
+                        So you think that you're a Friends fanatic? Test your knowledge of the
+                        show and the characters with this simple trivia game.
+                        Log in to play and track your score and compete with your friends!
+                    </p>
                     <label>Username:</label>
-                    <input type='text' value={this.state.username} onChange={this.handleUsernameChange} />
+                    <input type='text' value={this.username} onChange={this.handleUsername} />
                     <input type='submit' value='Log In' />
                 </form>
             </div>
