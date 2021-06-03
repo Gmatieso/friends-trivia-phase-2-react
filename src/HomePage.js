6354b8a88faf34f84c2b53b41eacd8b2a7ab6ab9
Christine Jeon <csjeon28@gmail.com>
2021-06-03 01:42:16 -0400

    added username functionality

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
 import './HomePage.css';
+import Trivia from './Trivia';
 
 class HomePage extends Component {
-    handleSubmit(e) {
-        e.preventDefault();
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            gameStart: false
+        }
+        this.startQuiz = this.startQuiz.bind(this)
     }
 
-    handleUsername(e) {
+    handleUsername = (e) => {
+        this.setState({
+            username: e.target.value
+        })
+    }
 
+    startQuiz() {
+        if (this.state.username !== '') {
+            this.setState({
+                gameStart: true
+            })
+        }
     }
 
     render() {
+        const { gameStart } = this.state;
+        if (gameStart === true) {
+            return (
+                <div>
+                    <Trivia username={this.state.username} />
+                </div>
+            )
+        }
         return (
             <div className='homepage'>
                 <div className='homepage-welcome'>
                     Welcome to the Friends Trivia Game!
                 </div>
-                <div className='homepage-image'>
-                    <img src='/images/Friends door.jpeg' alt='Friends door'></img>
-                </div>
+                <img src='/images/Friends door.jpeg' alt='Friends door'></img>
                 <form onSubmit={this.handleSubmit}>
                     <p>
                         So you think that you're a Friends fanatic? Test your knowledge of the
@@ -26,8 +48,14 @@ class HomePage extends Component {
                         Log in to play and track your score and compete with your friends!
                     </p>
                     <label>Username:</label>
-                    <input type='text' value={this.username} onChange={this.handleUsername} />
-                    <input type='submit' value='Log In' />
+                    <input type='text' value={this.state.username} onChange={this.handleUsername} />
+                    <input
+                        type='submit'
+                        value='Log In'
+                        className='homepage-username-button'
+                        disabled={this.state.username.length < 1}
+                        onClick={this.startQuiz}
+                    />
                 </form>
             </div>
         )
