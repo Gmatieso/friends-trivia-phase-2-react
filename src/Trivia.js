6698a8b5c4b17792007e06f86279d6f292ef97c4
Christine Jeon <csjeon28@gmail.com>
2021-06-03 02:48:38 -0400

    erased console logs and cleaned up other comments

--- src/Trivia.js
+++ src/Trivia.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import './Trivia.css';
+// import './Trivia.css';
 import axios from 'axios';
+import HomePage from './HomePage';
+import QuizQuestions from './QuizQuestions';
 
 const api_url = 'https://csjeon28.github.io/Data/db.json';
 
@@ -8,93 +10,47 @@ class Trivia extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            gameStart: false,
-            userAnswer: null,
-            score: 0,
-            gameReset: false,
-            questionNumber: 0,
-            totalQuestions: 0,
-            question: '',
-            correct: '',
-            answer1: '',
-            answer2: '',
-            answer3: '',
-            answer4: '',
+            username: '',
+            startQuiz: false,
+            endQuiz: false
         };
         this.startGame = this.startGame.bind(this);
-        this.loadNewQuestion = this.loadNewQuestion.bind(this);
-        // this.fetchQuestions = this.fetchQuestions.bind(this);
     }
 
-    // async componentDidMount() {
-    //     const triviaQuestions = await axios.get(api_url);
-    //     this.setState({ 
-    //         allQuestions: triviaQuestions.data 
-    //     });
-    // }
-    componentDidMount() {
-        this.fetchQuestions();
-    }
-
-    fetchQuestions() {
-        axios.get(api_url)
-            .then(response => {
-                this.setState({
-                    question: response.data
-                })
-            })
-    }
-
-    startGame = () => {
-        this.setState({
-            gameStart: true,
-            questionNumber: this.state.questionNumber + 1
-        });
-        this.loadNewQuestion();
-    }
-
-    loadNewQuestion = () => {
-        const { questionNumber } = this.state
-        this.setState({
-            question: [questionNumber].question,
-            answer1: [questionNumber].answer1,
-            answer2: [questionNumber].answer2,
-            answer3: [questionNumber].answer3,
-            answer4: [questionNumber].answer4,
-            correct: [questionNumber].correct,
-            totalQuestions: [questionNumber].length
+    async componentDidMount() {
+        let allData = await axios.get(api_url);
+        const questions = allData.data.quizData.map(q => {
+            return q.question
         })
-        this.nextQuestion();
-    }
-
-    nextQuestion = () => {
-        if (this.state.userAnswer === this.state.correct) {
-            this.setState({ score: this.state.score + 1 })
-        }
-    }
-
-    checkAnswer = (correct) => {
         this.setState({
-            userAnswer: correct,
+            questions: questions,
+            username: this.props.username
         })
-    }
+    };
 
-    finishGame = () => {
-        if (this.state.questionNumber === [this.state.questionNumber].length - 1) {
-            this.setState({
-                gameReset: true
-            })
-        }
-    }
-
-    handleClick = () => {
-        this.loadNewQuestion();
+    startGame() {
+        this.setState({
+            startQuiz: true
+        })
     }
 
     render() {
-
+        if (this.state.startQuiz === true) {
+            return (
+                <div>
+                    <QuizQuestions username={this.state.username} />
+                </div>
+            )
+        } else if (this.state.endQuiz === true) {
+            return (
+                <div>
+                    <HomePage />
+                </div>
+            )
+        }
         return (
             <div className='trivia'>
+                <header>Current User: {this.props.username}</header>
                 <div className='trivia-start-button'>
                     <button>Start Quiz</button>
                 </div>
@@ -108,14 +64,19 @@ class Trivia extends Component {
                         <div className='trivia-info'>3. Last but not least, don't cheat!</div>
                     </div>
                     <div className='trivia-buttons'>
-                        <button className='trivia-quit'>Quit Game</button>
-                        <button className='trivia-continue' onClick={this.handleClick}>Continue</button>
+                        <button
+                            className='trivia-quit'
+                            onClick={() => { this.setState({ username: '', endQuiz: true }) }}>
+                            Quit Game</button>
+                        <button
+                            className='trivia-continue'
+                            onClick={this.startGame}>
+                            Continue</button>
                     </div>
                 </div>
             </div>
         )
     }
 }
-
 
 export default Trivia;
