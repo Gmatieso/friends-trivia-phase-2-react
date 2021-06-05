303f6ade0d2c669ef4668b5611a7ab9edb878c43
Christine Jeon <csjeon28@gmail.com>
2021-06-05 01:57:23 -0400

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import Trivia from './Trivia';
-import Comments from './Comments';
 import './App.css';
 
 class App extends Component {
@@ -15,13 +14,11 @@ class App extends Component {
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/about'>About</Link></li>
             <li><Link to='/trivia'>Trivia</Link></li>
-            <li><Link to='/comments'>Comments</Link></li>
           </ul>
           <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/about' component={AboutPage} />
             <Route path='/trivia' component={Trivia} />
-            <Route path='/comments' component={Comments} />
           </Switch>
         </div>
       </Router>
