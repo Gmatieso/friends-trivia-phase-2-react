0de07e461f7156e2926d628aeb3d18d6bc2c5bfe
Christine Jeon <csjeon28@gmail.com>
2021-06-05 01:58:01 -0400

    worked on handleSubmit and added event prevent default

--- src/HomePage.js
+++ src/HomePage.js
@@ -9,6 +9,8 @@ class HomePage extends Component {
             username: '',
             gameStart: false
         }
+        this.handleUsername = this.handleUsername.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
         this.startTrivia = this.startTrivia.bind(this)
     }
 
@@ -18,7 +20,9 @@ class HomePage extends Component {
         })
     }
 
-    //figure out handleSubmit
+    handleSubmit(e) {
+        e.preventDefault();
+    }
 
     startTrivia() {
         if (this.state.username !== '') {
@@ -42,7 +46,6 @@ class HomePage extends Component {
                 <div className='homepage-welcome'>
                     Welcome to the Friends Trivia Game!
                 </div>
-                <img src='/images/Friends door.jpeg' alt='Friends door'></img>
                 <form onSubmit={this.handleSubmit}>
                     <p>
                         So you think that you're a Friends fanatic? Test your knowledge of the
@@ -52,12 +55,14 @@ class HomePage extends Component {
                     <label>Username:</label>
                     <input type='text' value={this.state.username} onChange={this.handleUsername} />
                     <input
-                        type='submit'
+                        type='button'
                         value='Log In'
                         className='homepage-username-button'
-                        disabled={this.state.username.length < 1}
+                        disabled={this.state.username.length < 4}
                         onClick={this.startTrivia}
                     />
+                    <h5>Your username must be at least 4 characters.</h5>
+                    <img src='/images/Friends door.jpeg' alt='Friends door'></img>
                 </form>
             </div>
         )
