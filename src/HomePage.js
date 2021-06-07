3df1c547bb15e022c6a6c5cd9c16a7ede86c382e
Christine Jeon <csjeon28@gmail.com>
2021-06-06 22:55:10 -0400

    fixed grammar in paragraph

--- src/HomePage.js
+++ src/HomePage.js
@@ -44,26 +44,41 @@ class HomePage extends Component {
         return (
             <div className='homepage'>
                 <div className='homepage-welcome'>
-                    Welcome to the Friends Trivia Game!
+                    <div>
+                        The One With The {"\n"}
+                    </div>
+                    <div className='homepage-friends-title'>
+                        F<span className='red-span'>&#183;</span>
+                        r<span className='blue-span'>&#183;</span>
+                        i<span className='yellow-span'>&#183;</span>
+                        e<span className='red-span'>&#183;</span>
+                        n<span className='blue-span'>&#183;</span>
+                        d<span className='yellow-span'>&#183;</span>
+                        s Trivia
+                    </div>
                 </div>
-                <form onSubmit={this.handleSubmit}>
-                    <p>
-                        So you think that you're a Friends fanatic? Test your knowledge of the
-                        show and the characters with this simple trivia game.
-                        Log in to play and track your score and compete with your friends!
-                    </p>
-                    <label>Username:</label>
-                    <input type='text' value={this.state.username} onChange={this.handleUsername} />
-                    <input
-                        type='button'
-                        value='Log In'
-                        className='homepage-username-button'
-                        disabled={this.state.username.length < 4}
-                        onClick={this.startTrivia}
-                    />
+                <img src='/images/Friendsdoor.jpeg' alt='Friends door'></img>
+                <div className='homepage-paragraph'>
+                    Do you think you're a Friends fanatic? Test your knowledge of the
+                    show and the characters with this simple trivia game.
+                    Create a username to play. You can see your score at the end!
+                    <form onSubmit={this.handleSubmit}>
+                        <label>Username:&nbsp;</label>
+                        <input
+                            className='homepage-input-box'
+                            type='text'
+                            value={this.state.username}
+                            onChange={this.handleUsername} />
+                        <input
+                            type='button'
+                            value='Log In'
+                            className='homepage-username-button'
+                            disabled={this.state.username.length < 4}
+                            onClick={this.startTrivia}
+                        />
+                    </form>
                     <h5>Your username must be at least 4 characters.</h5>
-                    <img src='/images/Friendsdoor.jpeg' alt='Friends door'></img>
-                </form>
+                </div>
             </div>
         )
     }
